aac933bfb5e992878bdc4fae76af3a10ce857da9
Simon Burgoyne <simon.burgoyne@7im.co.uk>
2022-10-05 15:47:33 +0100

    File and dir renames

--- lib/tools/gherkin_feature_testing/bdd_report.dart
+++ lib/tools/gherkin_feature_testing/bdd_report.dart
@@ -16,7 +16,7 @@ const String iPhone8UUID = "6FB30977-E11C-4001-A749-4403F4B3E261";
 const String iPhone13ProUUID = "A49BC245-DB99-4E14-9870-131C8976CA2C";
 const String iPhone14ProUUID = "0A2BDB91-D9C7-4897-94EA-C48A499CCE19";
 const String pixel5EmulatorID = "emulator-5554";
-final String gherkinReportPath = "${Directory.current.path}/integration_test/reports/integration_response_data.json";
+final String gherkinReportPath = "${Directory.current.path}/integration_test/report/integration_response_data.json";
 
 final deviceUuidMap = {
     iPhone13Pro: iPhone13ProUUID,
@@ -64,7 +64,7 @@ Future<void> deleteExistingReport() async {
 
 Future<void> runFeatureTests(String deviceId) async {
   stdout.writeln("   Running Application Feature Tests... DeviceId: $deviceId");
-  stdout.writeln("      Cmd: flutter drive -driver=test_driver/integration_test_feature_driver.dart --target=integration_test/gherkin_suite_recovery_test.dart -d $deviceId");
+  stdout.writeln("      Cmd: flutter drive -driver=test_driver/integration_test_feature_driver.dart --target=integration_test/gherkin_suite_test.dart -d $deviceId");
   await runCommand(flutterCmd, [...runTheFeatureTests, "-d", deviceId]);
 }
 
@@ -73,7 +73,7 @@ void createCucumberJsReport(String device) {
   var jsonReportPath = gherkinReportPath;
   var jsonReport = File(jsonReportPath).readAsStringSync();
   var formattedJson = formatJsonForCucumberHtmlReport(jsonReport);
-  var formattedReportPath = "${Directory.current.path}/integration_test/gherkin/reporters/cucumber-html/digestableme.json";
+  var formattedReportPath = "${Directory.current.path}/integration_test/gherkin/reporter/cucumber-html/digestableme.json";
   File(formattedReportPath).writeAsStringSync(formattedJson);
 }
 
@@ -90,12 +90,12 @@ void createReportMetaData(String deviceName){
 }
   ''';
   metaJson = metaJson.replaceAll("deviceId", deviceName);
-  var metaPath = "${Directory.current.path}/integration_test/gherkin/reporters/cucumber-html/meta.json";
+  var metaPath = "${Directory.current.path}/integration_test/gherkin/reporter/cucumber-html/meta.json";
   File(metaPath).writeAsStringSync(metaJson);
 }
 
 Future<void> viewCucumberJsReport(String path) async {
   stdout.writeln("   Opening Cucumberjs Report");
-  Directory.current = "$path/integration_test/gherkin/reporters/cucumber-html";
+  Directory.current = "$path/integration_test/gherkin/reporter/cucumber-html";
   await runCommand(nodeCmd, ["index.js"]);
 }
