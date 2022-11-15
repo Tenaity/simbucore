aee8c3d48379506832b0876abdb62fdacd102882
Simon Burgoyne <simon.burgoyne@7im.co.uk>
2022-11-15 09:14:21 +0000

    Resolve riverpod providers without a WidgetRef

--- lib/app/core/service/provider_resolver.dart
+++ lib/app/core/service/provider_resolver.dart
@@ -3,12 +3,12 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 /// Uses the root WidgetRef to resolve providers without a Riverpod WidgetRef.
 class ProviderResolver{
   static late WidgetRef _ref;
-  bool _hasWidgetRef = false;
+  static bool _hasWidgetRef = false;
 
-  WidgetRef get ref {
+  static WidgetRef get ref {
     assert(!_hasWidgetRef, "RiverpodResolver: WidgetRef not available to resolve providers, the root widget must build before we can use this service.");
     return _ref;
   }
 
-  set widgetRef(WidgetRef widgetRef) => {_ref = widgetRef, _hasWidgetRef = true};
+  static set widgetRef(WidgetRef widgetRef) => {_ref = widgetRef, _hasWidgetRef = true};
 }
