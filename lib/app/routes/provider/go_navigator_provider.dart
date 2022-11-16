1f36edf80eb7ae21e44dd0cfd56949daa4b10dab
Simon Burgoyne <simon.burgoyne@7im.co.uk>
2022-11-16 14:28:14 +0000

    Raise navigated events when routing

--- lib/app/routes/provider/go_navigator_provider.dart
+++ lib/app/routes/provider/go_navigator_provider.dart
@@ -1,7 +1,8 @@
 // Package imports:
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:simbucore/app/event_store/provider/event_provider.dart';
 import 'package:simbucore/app/routes/service/go_navigator.dart';
 
 final goNavigatorProvider = Provider<GoNavigator>(
-  (ref) => GoNavigator(),
+  (ref) => GoNavigator(ref.read(eventStoreProvider)),
 );
