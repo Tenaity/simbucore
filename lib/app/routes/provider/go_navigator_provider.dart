159c249360ed78ef53041394914fbac50bd9e6f1
Simon Burgoyne <simon.burgoyne@7im.co.uk>
2022-11-16 14:34:06 +0000

    Back out navigated events

--- lib/app/routes/provider/go_navigator_provider.dart
+++ lib/app/routes/provider/go_navigator_provider.dart
@@ -1,8 +1,7 @@
 // Package imports:
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:simbucore/app/event_store/provider/event_provider.dart';
 import 'package:simbucore/app/routes/service/go_navigator.dart';
 
 final goNavigatorProvider = Provider<GoNavigator>(
-  (ref) => GoNavigator(ref.read(eventStoreProvider)),
+  (ref) => GoNavigator(),
 );
--- lib/app/routes/service/go_navigator.dart
+++ lib/app/routes/service/go_navigator.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
-import 'package:simbucore/app/event_store/service/event_store.dart';
-import 'package:simbucore/app/routes/event/navigated.dart';
 
 // Enable navigation without the BuildContext using a global navigationKey.
 class GoNavigator{
-  final EventStore _eventStore;
-  
   static late BuildContext _buildContext;
   static bool _hasBuildContext = false;
-
-  GoNavigator(this._eventStore);
 
   static BuildContext get buildContext {
     assert(!_hasBuildContext, "NoContextNavigator: BuildContext is not available to resolve providers, the root widget must initialise before we can use this service.");
@@ -21,16 +15,13 @@ class GoNavigator{
 
   void push(String routeName){
     GoRouter.of(_buildContext).push(routeName);
-    _eventStore.bus.fire(Navigated(routeName));
   }
 
   void go(String routeName){
     GoRouter.of(_buildContext).go(routeName);
-    _eventStore.bus.fire(Navigated(routeName));
   }
 
   void pop(){
     GoRouter.of(_buildContext).pop();
-    _eventStore.bus.fire(Navigated("pop"));
   }
 }
