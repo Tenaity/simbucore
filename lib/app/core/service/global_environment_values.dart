feb9fac3978abf1fe5d6faa05c3150927c975465
Simon Burgoyne <simon.burgoyne@7im.co.uk>
2022-12-01 11:44:51 +0000

    Revert "Environment Tag (Gherkin Feature Testing)"
    This reverts commit ce7f73cb3bf895594c014f9b8f216ea42fdc8a6c.

--- lib/app/core/service/global_environment_values.dart
+++ lib/app/core/service/global_environment_values.dart
@@ -9,17 +9,15 @@ class GlobalEnvironmentValues{
   static final instance = GlobalEnvironmentValues._();
 
   late EnvironmentValues _values;
-  bool initialised = false;
   
-  String get apiEndpoint => _values.apiEndpoint;
-  Environments get environment => _values.environment;
-  
+  get apiEndpoint => _values.apiEndpoint;
+  get environment => _values.environment;
+
   void loadValues(String jsonEnvironmentValues){
     var reader = EnvConfigReader(jsonEnvironmentValues);
     var apiEndpoint = reader.value("api_endpoint_url");
     var environment = _getEnvironment(reader.value("environment"));
     _values = EnvironmentValues(apiEndpoint, environment);
-    initialised = true;
   }
   
   Environments _getEnvironment(String value) {
